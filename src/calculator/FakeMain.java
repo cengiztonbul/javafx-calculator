e8e25ccd9c6ca1c2a01e4fae961bb3507bc03204
cengiztonbul <cengiztonbul@outlook.com>
2021-01-05 15:13:22 +0300

    comments

--- src/calculator/FakeMain.java
+++ src/calculator/FakeMain.java
@@ -1,5 +1,7 @@
 package calculator;
 
+// jar build failed with intellij due to a bug in the IDE
+// this class runs the actual main
 public class FakeMain
 {
     public static void main(String[] args)
