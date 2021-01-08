6717ce1413c18d45aa50809e7fc070bd89e9251e
cengiztonbul <cengiztonbul@outlook.com>
2021-01-09 00:07:24 +0300

    rename files

--- src/calculator/BuildMain.java
+++ src/calculator/BuildMain.java
@@ -2,10 +2,10 @@ package calculator;
 
 // jar build failed with intellij due to a bug in the IDE
 // this class runs the actual main
-public class FakeMain
+public class BuildMain
 {
     public static void main(String[] args)
     {
-        calculator.Main.main(args);
+        Main.main(args);
     }
 }
